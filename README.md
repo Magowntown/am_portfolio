daea9d14b154891669038fa3902b5a048569d52f
Magowntown <amagown@gmail.com>
2022-05-03 18:17:32 -0400

    Sanity.io schemas | modules | react frontend start

---
+++ README.md
@@ -0,0 +1,2 @@
+# am_portfolio
+Andrew Magown's personal portfolio page for showcasing coding experience and skills
---
+++ backend_sanity/schemas/experiences.js
@@ -0,0 +1,17 @@
+export default {
+    name: 'experiences',
+    title: 'Experiences',
+    type: 'document',
+    fields: [{
+            name: 'year',
+            title: 'Year',
+            type: 'string'
+        },
+        {
+            name: 'works',
+            title: 'Works',
+            type: 'array',
+            of: [{ type: 'experiences' }]
+        },
+    ]
+}
